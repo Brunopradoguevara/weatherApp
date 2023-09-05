adf86190ab216e5aec427c7ec12c70f3024c8542
Bruno Prado <103675938+Brunopradoguevara@users.noreply.github.com>
2023-09-05 14:27:51 -0500

    making modal alert

--- src/App.jsx
+++ src/App.jsx
@@ -14,25 +14,25 @@ function App() {
   const [temp, setTemp] = useState()
   const [image, setImage] = useState()
   const [inputValue, setInputValue] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  const [isAlertOpen, setIsAlertOpen] = useState(false) 
 
-  //loader
-  useEffect(() => {
-    const loader = document.querySelector('.loader');
-    loader.classList.add('loader--hidden');
-  }, [inputValue]);
   
     //Api navigator
    useEffect(() => {
+    
     const success = pos =>{
       const obj = {
         lat: pos.coords.latitude,
         lon: pos.coords.longitude
       }
       setCoords(obj) 
+      
     }
     const error = () => {
+      setIsAlertOpen(true)
       setInputValue("london")
-      console.log(inputValue)
+      
     }
     navigator.geolocation.getCurrentPosition(success,error)
 
@@ -40,13 +40,14 @@ function App() {
 
       //Api geo
      useEffect(()=>{
+      setIsLoading(true)
       if(inputValue){
         const apiKey = "43be0da6a4d12a1f3d653a5f3389d21b"
         const cityName= inputValue
         const url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
         axios.get(url)
         .then(res =>{
-          console.log(res.data[0])
+          setIsLoading(false)
           const obj = {
           lat: res.data[0].lat,
           lon: res.data[0].lon
@@ -59,12 +60,14 @@ function App() {
 
     // Api weatherApi
   useEffect(()=>{
+    setIsLoading(true)
     if(coords){
       const apiKey = "43be0da6a4d12a1f3d653a5f3389d21b"
       const units = "metric"
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&units=${units}&appid=${apiKey}`  
       axios.get(url)
       .then(res => {
+        setIsLoading(false)
         setWeather(res.data)
         const obj = {
           celsius: (res.data.main.temp).toFixed(1),
@@ -79,19 +82,25 @@ function App() {
 
   //Api pixabay
   useEffect(()=>{
+    setIsLoading(true)
     const apiKey = "39164433-94ec2e1273f88ab80880706f0"
     const url = `https://pixabay.com/api/?key=${apiKey}&q=${weather?.weather[0].description}`
     if(weather?.weather[0].description==="moderate rain"){
       const newUrl=`https://pixabay.com/api/?key=${apiKey}&q=rain}`
       axios.get(newUrl)
-      .then(res => setImage(res.data))
+      .then(res => {
+        setIsLoading(false)
+        setImage(res.data)
+      })
       .catch(err => console.log(err))
     }else{
       axios.get(url)
-      .then(res => setImage(res.data))
+      .then(res => {
+        setIsLoading(false)
+        setImage(res.data)
+      })
       .catch(err => console.log(err))
     }
-    console.log(weather?.weather[0].description)
     
   },[weather])
 
@@ -99,7 +108,6 @@ function App() {
     backgroundImage: `url(${image?.hits[0]?.largeImageURL})`,
     backgroundSize: 'cover',
   }
-  console.log(image?.hits[0]?.largeImageURL)
   const inputSearch = useRef()
 
   const handleSubmit = (e)=>{
@@ -110,23 +118,34 @@ function App() {
   }
 
   return (
-    <div style={objStile} className='container'>
-      <div className="content__container">  
+    <div style={objStile} className='container'>  
+      {isLoading ? (
         <div class="loader">
-          <div class='loader__container'></div>
+          <div class='loader__container'>
+            <h2 className='loading__title'>Loading...</h2>
+            <div className='loader__animation--container'>
+             <div class='loader__animation'></div>
+            </div>
+          </div>
         </div>
-        <div className='form__container'>
-          <form className='form--search-city' onSubmit={handleSubmit}>
-            <input className='input--search-city' ref={inputSearch}  type='text'placeholder='Search for another city'  />
-            <button className='btn--search-city'>Search🔍</button>
-          </form>
+      ):(
+        <div className="content__container">
+          <div className='form__container'>
+            <form className='form--search-city' onSubmit={handleSubmit}>
+              <input className='input--search-city' ref={inputSearch}  type='text'placeholder='Search for another city'/>
+              <button className='btn--search-city'>Search🔍</button>
+            </form>
+          </div>
+          <WeatherCard
+            weather = {weather}
+            temp = {temp}/>
+          <ModalAlert
+            isAlertOpen={isAlertOpen}
+            setIsAlertOpen={setIsAlertOpen}
+          />
+
         </div>
-        <WeatherCard
-          weather = {weather}
-          temp = {temp}
-        />
-        <ModalAlert/>
-      </div>
+      )}
     </div>
   )
 }
--- src/components/ModalAlert.jsx
+++ src/components/ModalAlert.jsx
@@ -1,14 +1,30 @@
 
-const ModalAlert = () => {
-    
+const ModalAlert = ({isAlertOpen,setIsAlertOpen}) => {
 
+  const handleAlert = ()=>{
+    setIsAlertOpen(false)
+  }
 
   return (
-    <div>
-        <dialog>
-        <ion-icon name="alert-circle-outline"></ion-icon>
-
+    <div className="alert__section">
+      {isAlertOpen ? (
+        <dialog open className="alert">
+        <div className="alert__content"> 
+          <ion-icon name="alert-circle-outline"></ion-icon>
+          <span className="alert__text">Acceso a la ubicación denegada</span>
+          <button onClick={handleAlert} className="btn--close_modal">Aceptar</button>
+        </div>
+      </dialog>
+      ) : (
+        <dialog  className="alert">
+          <div className="alert__content"> 
+            <ion-icon name="alert-circle-outline"></ion-icon>
+            <span className="alert__text">Acceso a la ubicación denegada</span>
+            <button onClick={handleAlert} className="btn--close_modal">Aceptar</button>
+          </div>
         </dialog>
+      )}
+        
     </div>
   )
 }
